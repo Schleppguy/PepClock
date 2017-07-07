65ce02cfd67fee654e5aeecfcb543e1f3573b95d
Aaron Xavier <aarondxavier@gmail.com>
2017-07-07 12:04:10 -0700

    App loads with addition of store.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './components/Routes';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
+import reducers from './reducers';
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
+ReactDOM.render(<Provider store={createStoreWithMiddleware(reducers)}>
+    <Routes />
+  </Provider>, document.getElementById('root'));
